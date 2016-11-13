f8268c1aa7fe5db1277632f780a36a9720e1cecd
sterenziani <sterenziani@itba.edu.ar>
2016-11-13 18:55:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,9 @@
-typedef struct {
-    int turno;
-    int ptsjug1;
-    int ptsjug2;
-    char **tablero;
-    int dim;
-    int jugadores;
-}tPartida;
+#include "buttonsAndScissorsBack.h"
 
-typedef struct {
-    int F1;
-    int C1;
-    int F2;
-    int C2;
-}tMovimiento;
-
-
-
-
-
+int menu();
 
 int main()
 {
-    tPartida partida;
-    tMovimiento movimiento;
     menu();
-    ingrese_comando(&movimiento,&partida);
     return 0;
 }
